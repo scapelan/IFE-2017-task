654dd8a7b62b9318ada30968822b8b4b56f8d89e
Scapelan <scapelan@qq.com>
2017-11-25 20:12:44 +0800

    完成task2

--- JavaScript/task2/task2.js
+++ JavaScript/task2/task2.js
@@ -1,8 +1,27 @@
 var aqiData = [
-    ["北京", 90],
-    ["上海", 50],
-    ["武汉", 10],
-    ["广州", 50],
-    ["成都", 90],
-    ["西安", 100]
-  ];
+  ["北京", 90],
+  ["上海", 50],
+  ["武汉", 10],
+  ["广州", 50],
+  ["成都", 90],
+  ["西安", 100]
+];
+// 将aqiData数组从大到小排序
+aqiData = aqiData.sort(function(a, b){
+  if(a[1]>b[1]){ 
+    return -1;
+  }
+  if(a[1]<b[1]){
+    return 1;
+  }
+  return 0; 
+})
+// 生成列表项
+var aqiList = "";
+for(let item = 0; item < aqiData.length; item++){
+  if(aqiData[item][1] > 60){
+    aqiList += "<li>" + "第" + (item+1) + "名：" + aqiData[item][0] + ", " + aqiData[item][1] + "</li>"; 
+  }
+}
+// 将列表项填进去
+document.getElementById("aqi-list").innerHTML = aqiList;
